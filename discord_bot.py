e6d47243437c5cae99eafadc3825b5fc8b0dd0a4
LunarcatOwO <54731257+LunarcatOwO@users.noreply.github.com>
2024-04-04 13:15:53 -0400

    updated code structure again

--- discord_bot.py
+++ discord_bot.py
@@ -1,35 +1,14 @@
-from flask import Flask, request, jsonify
-from threading import Thread
+import time
 import subprocess
 import os
-import sys
 
-app = Flask(__name__)
+while True:
+    print("Starting bot and webserver...")
+    bot_process = subprocess.Popen(['python', 'webserver.py'])
 
-def run_discord_bot():
-    import src.main
-    pass
+    while not os.path.exists('restart.txt'):
+        time.sleep(1)
 
-@app.route('/Iseal-Discord-Bot', methods=['POST'])
-def handle_webhook():
-    try:
-        payload = request.json
-        if 'ref' in payload and payload['ref'] == 'refs/heads/master':
-            print("Received a push to the master branch. Fetching latest code...")
-            subprocess.run(['git', 'pull'])
-            print("Code fetched successfully!")
-
-            # Restart the script
-            os.execv(sys.executable, ['python'] + sys.argv)
-
-    except Exception as e:
-        return jsonify({'error': str(e)})
-    return jsonify({'message': 'success'})
-
-if __name__ == '__main__':
-    # Start the discord bot in a separate thread
-    bot_thread = Thread(target=run_discord_bot)
-    bot_thread.start()
-
-    # Start the server
-    app.run(host='0.0.0.0', port=8080)
+    print("Restart signal received. Restarting bot...")
+    bot_process.terminate()
+    os.remove('restart.txt')
---
+++ webserver.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from threading import Thread
+import subprocess
+import os
+import sys
+
+app = Flask(__name__)
+
+def run_discord_bot():
+    import src.main
+    pass
+
+@app.route('/Iseal-Discord-Bot', methods=['POST'])
+def handle_webhook():
+    try:
+        payload = request.json
+        if 'ref' in payload and payload['ref'] == 'refs/heads/master':
+            print("Received a push to the master branch. Fetching latest code...")
+            subprocess.run(['git', 'pull'])
+            print("Code fetched successfully!")
+
+            # Signals watcher to restart script
+            open('restart.txt', 'w').close()
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    return jsonify({'message': 'success'})
+
+if __name__ == '__main__':
+    # Start the discord bot in a separate thread
+    bot_thread = Thread(target=run_discord_bot)
+    bot_thread.start()
+
+    # Start the server
+    app.run(host='0.0.0.0', port=8080)
