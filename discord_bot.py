40d80b225f8fcda630e0497ed9a6564ddc41882b
LunarcatOwO <54731257+LunarcatOwO@users.noreply.github.com>
2024-04-17 19:29:28 -0400

    removed the webserver system as it was causing problems, alot of them.

--- discord_bot.py
+++ discord_bot.py
@@ -1,17 +1,1 @@
-import time
-import subprocess
-import os
-
-
-while True:
-    print("Starting bot and webserver...")
-    bot_process = subprocess.Popen(['python3', 'webserver.py'])
-
-    while not os.path.exists('restart.txt'):
-        time.sleep(1)
-
-    print("Restart signal received. Restarting bot...")
-    bot_process.terminate()
-    bot_process.wait()
-    os.remove('restart.txt')
-    print("Bot restarted.")
+import src.main
