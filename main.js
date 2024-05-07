be53a433374d4fd669f6a82d76cb1831c715f3a9
LunarcatOwO <54731257+LunarcatOwO@users.noreply.github.com>
2024-05-07 13:09:54 -0400

    /config

--- main.js
+++ main.js
@@ -51,6 +51,10 @@ const commands = [
     name: "rules",
     description: "Get the server's rules",
   },
+  {
+    name: "config",
+    description: "Get how to acess the config file",
+  },
 ];
 
 const rest = new REST({ version: "10" }).setToken(TOKEN);
@@ -101,6 +105,10 @@ client.on("interactionCreate", async (interaction) => {
         iconURL:
           "https://cdn.discordapp.com/avatars/905758994155589642/c6784ea84cd89d2ec67688b97f04c922?size=1024&f=.png",
       });
+    if (!interaction.guild) {
+      await interaction.reply({ embeds: [embed] });
+      return;
+    }
     const member =
       interaction.member ||
       (await interaction.guild.members.fetch(interaction.user.id));
@@ -130,6 +138,9 @@ client.on("interactionCreate", async (interaction) => {
           "https://cdn.discordapp.com/avatars/905758994155589642/c6784ea84cd89d2ec67688b97f04c922?size=1024&f=.png",
       });
     const roleNamesToCheck = ["ISeal", "Community Manager"];
+    const member =
+      interaction.member ||
+      (await interaction.guild.members.fetch(interaction.user.id));
     const hasRole = member.roles.cache.some((role) =>
       roleNamesToCheck.includes(role.name)
     );
@@ -143,14 +154,23 @@ client.on("interactionCreate", async (interaction) => {
     const embed = new EmbedBuilder()
       .setColor("#0099ff")
       .setTitle("Resource Pack")
-      .setDescription(`[Click me to download the default resourcepack for PowerGems](https://cdn.discordapp.com/attachments/1157658269318402058/1193993804672421918/Powergems_Pack.zip?ex=662a00a2&is=6628af22&hm=ab75523cb14d11b57debef3faa3616e111e2f57d7a4674131d8d59740eeeba10&) 
-[Click me to download the magic resource pack for PowerGems](https://cdn.discordapp.com/attachments/1157658269318402058/1194529976645582858/PowerGems_magic_pack.zip?ex=6629f9bb&is=6628a83b&hm=157d405d2f872ff9365371da79c110c793cb6e89178a84b3d508e4f65c9f7218&)`)
+      .setDescription(
+        `[Click me to download the default resourcepack for PowerGems](https://cdn.discordapp.com/attachments/1157658269318402058/1193993804672421918/Powergems_Pack.zip?ex=662a00a2&is=6628af22&hm=ab75523cb14d11b57debef3faa3616e111e2f57d7a4674131d8d59740eeeba10&) 
+[Click me to download the magic resource pack for PowerGems](https://cdn.discordapp.com/attachments/1157658269318402058/1194529976645582858/PowerGems_magic_pack.zip?ex=6629f9bb&is=6628a83b&hm=157d405d2f872ff9365371da79c110c793cb6e89178a84b3d508e4f65c9f7218&)`
+      )
       .setTimestamp()
       .setFooter({
         text: "Made with ❤️ by lunarcatowo",
         iconURL:
           "https://cdn.discordapp.com/avatars/905758994155589642/c6784ea84cd89d2ec67688b97f04c922?size=1024&f=.png",
       });
+    if (!interaction.guild) {
+      await interaction.reply({ embeds: [embed] });
+      return;
+    }
+    const member =
+    interaction.member ||
+    (await interaction.guild.members.fetch(interaction.user.id));
     const roleNamesToCheck = ["ISeal", "Community Manager"];
     const hasRole = member.roles.cache.some((role) =>
       roleNamesToCheck.includes(role.name)
@@ -174,7 +194,7 @@ client.on("interactionCreate", async (interaction) => {
         {
           name: "2️⃣ Help us to help you!",
           value:
-            "If you are reporting a bug/issue, please give plenty of information. Simply just saying \"Help\" isn't very useful.",
+            'If you are reporting a bug/issue, please give plenty of information. Simply just saying "Help" isn\'t very useful.',
         },
         {
           name: "3️⃣ We have tickets!",
@@ -198,6 +218,13 @@ client.on("interactionCreate", async (interaction) => {
         iconURL:
           "https://cdn.discordapp.com/avatars/905758994155589642/c6784ea84cd89d2ec67688b97f04c922?size=1024&f=.png",
       });
+    if (!interaction.guild) {
+      await interaction.reply({ embeds: [embed] });
+      return;
+    }
+    const member =
+    interaction.member ||
+    (await interaction.guild.members.fetch(interaction.user.id));
     const roleNamesToCheck = ["ISeal", "Community Manager"];
     const hasRole = member.roles.cache.some((role) =>
       roleNamesToCheck.includes(role.name)
@@ -208,43 +235,97 @@ client.on("interactionCreate", async (interaction) => {
       await interaction.reply({ embeds: [embed], ephemeral: true });
     }
   }
-});
-
-client.on('threadCreate', async (thread) => {
-  await thread.join();
-  const embed = new EmbedBuilder()
-      .setColor("#FFFF00")
-      .setTitle("Hello!")
-      .setDescription("**I am a bot, I may not be able to assist you, but please be patient as someone will respond as soon as they can!**")
+  if (interaction.commandName === "config") {
+    
+    const embed = new EmbedBuilder()
+      .setColor("#0099ff")
+      .setTitle("How to access the config file")
+      .setDescription(
+        "Follow the following steps if you do not know how to access the config file"
+      )
+      .addFields(
+        {
+          name: "Step 1",
+          value: "Go to your server's file manager",
+          inline: true,
+        },
+        { name: "Step 2", value: "Open the `~/plugins` folder", inline: true },
+        {
+          name: "Step 3",
+          value: "Open the folder with the plugin's name. example of the route: `~/plugins/PowerGems`",
+          inline: true,
+        },
+        {
+          name: "Step 4",
+          value: "Open the `config.yml` file",
+          inline: true,
+        }
+      )
       .setTimestamp()
       .setFooter({
         text: "Made with ❤️ by lunarcatowo",
         iconURL:
           "https://cdn.discordapp.com/avatars/905758994155589642/c6784ea84cd89d2ec67688b97f04c922?size=1024&f=.png",
       });
-  await thread.send({ embeds: [embed]})
-  await thread.send({content: "<@905758994155589642> <@398908171357519872>"})
-  await thread.lastMessage.delete(); 
+      if (!interaction.guild) {
+        await interaction.reply({ embeds: [embed] });
+        return;
+      }
+      const member =
+      interaction.member ||
+      (await interaction.guild.members.fetch(interaction.user.id));
+      const roleNamesToCheck = ["ISeal", "Community Manager"];
+      const hasRole = member.roles.cache.some((role) =>
+        roleNamesToCheck.includes(role.name)
+      );
+      if (hasRole) {
+        await interaction.reply({ embeds: [embed] });
+      } else {
+        await interaction.reply({ embeds: [embed], ephemeral: true });
+      }
+  }
 });
 
-client.on('messageCreate', async (message) => {
+client.on("threadCreate", async (thread) => {
+  await thread.join();
+  const embed = new EmbedBuilder()
+    .setColor("#FFFF00")
+    .setTitle("Hello!")
+    .setDescription(
+      "**I am a bot, I may not be able to assist you, but please be patient as someone will respond as soon as they can!**"
+    )
+    .setTimestamp()
+    .setFooter({
+      text: "Made with ❤️ by lunarcatowo",
+      iconURL:
+        "https://cdn.discordapp.com/avatars/905758994155589642/c6784ea84cd89d2ec67688b97f04c922?size=1024&f=.png",
+    });
+  await thread.send({ embeds: [embed] });
+  await thread.send({ content: "<@905758994155589642> <@398908171357519872>" });
+  await thread.lastMessage.delete();
+});
+
+client.on("messageCreate", async (message) => {
   if (message.mentions.has(client.user)) {
-    await message.reply("**I am a bot, cannot assist you! If you want to report a bug put it in https://discord.com/channels/1157645386480091156/1157659553345831012 if you have a suggestion put it in https://discord.com/channels/1157645386480091156/1157664317932584970 **");
+    await message.reply(
+      "**I am a bot, cannot assist you! If you want to report a bug put it in https://discord.com/channels/1157645386480091156/1157659553345831012 if you have a suggestion put it in https://discord.com/channels/1157645386480091156/1157664317932584970 **"
+    );
   }
-  if (message.channel.type === 'DM' && message.author.id !== client.user.id) {
-    await message.reply("**I am a bot, cannot assist you! If you want to report a bug put it in https://discord.com/channels/1157645386480091156/1157659553345831012 if you have a suggestion put it in https://discord.com/channels/1157645386480091156/1157664317932584970 **");
+  if (message.channel.type === "DM" && message.author.id !== client.user.id) {
+    await message.reply(
+      "**I am a bot, cannot assist you! If you want to report a bug put it in https://discord.com/channels/1157645386480091156/1157659553345831012 if you have a suggestion put it in https://discord.com/channels/1157645386480091156/1157664317932584970 **"
+    );
   }
-  // if (message.channel.isThread() && !message.channel.members.has(client.user.id)) {
-  //   await message.channel.join();
-  // }
 });
 
-client.on('guildMemberAdd', async (member) => {
+client.on("guildMemberAdd", async (member) => {
   try {
     const embed = new EmbedBuilder()
       .setColor("#0099ff")
       .setTitle("Welcome to the server!")
-      .setDescription("Welcome to the server! if you are looking for the resource pack then run /resourcepack or /resources and it will automatically help you with that. Again welcome to the server and if you have any questions then just ask!")
+      .setDescription(
+        "Welcome to the server! if you are looking for the resource pack then run /resourcepack or /resources and it will automatically help you with that. Again welcome to the server and if you have any questions then just ask!"
+      )
 
       .setTimestamp()
       .setFooter({
@@ -252,7 +333,7 @@ client.on('guildMemberAdd', async (member) => {
         iconURL:
           "https://cdn.discordapp.com/avatars/905758994155589642/c6784ea84cd89d2ec67688b97f04c922?size=1024&f=.png",
       });
-    await member.user.send({embed:[embed]});
+    await member.user.send({ embed: [embed] });
   } catch (error) {
     console.error(`Could not send welcome DM to ${member.displayName}.`, error);
   }
